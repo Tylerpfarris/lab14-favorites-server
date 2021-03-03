3169a7eb69f7faadc8e740a9e39d6fc383d3bd67
Tyler Farris <Tylerpfarris@gmail.com>
2021-03-03 14:58:56 -0800

    local routs working

---
+++ data/favorites.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    drinkId: 11007,
+    name: 'Margarita',
+    glass: 'Cocktail glass',
+    image:'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
+  },
+  {
+    drinkId: 17241,
+    name: 'Zombie',
+    glass: 'Hurricane glass',
+    image:'https://www.thecocktaildb.com/images/media/drink/2en3jk1509557725.jpg',
+  },
+  {
+    drinkId: 17218,
+    name: 'Vesper',
+    glass: 'Cocktail glass',
+    image:'https://www.thecocktaildb.com/images/media/drink/mtdxpa1504374514.jpg',
+  },
+  
+];
